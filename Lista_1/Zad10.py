dcb965477a08f9d69ad3993c3139a49cfada1b0e
Deny35 <kdenys141@gmail.com>
2021-10-13 21:13:49 +0200

    nowe

--- Lista_1/Zad10.py
+++ Lista_1/Zad10.py
@@ -5,18 +5,14 @@ y = 1
 
 z = complex(x, y)
 
-print("rzeczywiste")
-print(cmath.sin(z).real)
-print(cmath.cos(z).real)
+print('Część rzeczywista:')
+print('sin:   ', cmath.sin(z).real)
+print('cos:   ', cmath.cos(z).real)
 
-print("urojone")
-print(cmath.sin(z).imag)
-print(cmath.cos(z).imag)
+print('Część urojona:')
+print('sin:   ', cmath.sin(z).imag)
+print('cos:   ', cmath.cos(z).imag)
 
 a = cmath.sin(z) ** 2 + cmath.cos(z) ** 2
-print(a)
-if (a==1):
-    print('Tak, założenie sin^2(z)+cos^2(z)=1 jest spaełnione.')
-else:
-    print('Nie jest spełnione.')
+print('Tak, założenie sin^2(z)+cos^2(z)=', a, 'jest spaełnione.')
 
