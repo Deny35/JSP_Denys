e265ad26262a809e1d46bd8ee8eabb3f2768246c
Deny35 <kdenys141@gmail.com>
2021-10-14 08:51:37 +0200

    Nowe

--- Lista_1/Zad9.py
+++ Lista_1/Zad9.py
@@ -3,6 +3,6 @@ import cmath
 
 x = float(input("Podaj część rzeczywistą "))
 y = float(input("Podaj część urojoną"))
-z = complex(x, y)
-print('Moduł:', math.sqrt(x**2 + y**2))
-print('Argument:', cmath.phase(z))
+z = complex(x, y) # Zamienia na zwspoloną
+print('Moduł:', math.sqrt(x**2 + y**2)) # Zwraca moduł
+print('Argument:', cmath.phase(z)) #Zwraca argument
