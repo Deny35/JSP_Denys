b0270265f7c00697bdfb2ce1286acb1b45ea2e77
Deny35 <kdenys141@gmail.com>
2021-10-14 08:50:08 +0200

    Nowe

--- Lista_1/Zad6.py
+++ Lista_1/Zad6.py
@@ -1,5 +1,5 @@
 #zad.6
 import math
 
-x = complex((-17)**0.5)
+x = complex((-17)**0.5, 0) # Zamiana na liczbe zespoloną
 print(x)
--- Lista_1/Zad8.py
+++ Lista_1/Zad8.py
@@ -2,6 +2,6 @@
 print('Napisz liczbe a i b,aby a>b')
 a = float(input('Liczba a:'))
 b = float(input('Liczba b:'))
-Z = b/a
+Z = b%a
 Z += 3
 print(Z)
