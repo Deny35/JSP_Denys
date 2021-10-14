c3635672dcddbfd995b3021be177524fc92b775a
Deny35 <kdenys141@gmail.com>
2021-10-14 08:35:07 +0200

    nowy

--- Lista_1/Zad5.py
+++ Lista_1/Zad5.py
@@ -1,6 +1,6 @@
 #zad.5
 import math
 print(7//3)
-print(round(7/3))
-print(math.floor(7/3))
+print(round(13/5))
+print(math.floor(13/5))
 # Różnica jest taka, że round zaokrągla do 0.5 w dół, a od 0.5 w górę, floor zaokrągla tylko w dół, a  // podaje tylko liczbę całkowitą liczbe z dzielenia
