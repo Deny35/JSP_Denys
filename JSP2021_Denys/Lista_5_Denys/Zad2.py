90a5cb3948d8713f55adf8077e4463cba6b9a487
Deny35 <kdenys141@gmail.com>
2021-10-28 09:06:31 +0200

    28.10.2021

--- JSP2021_Denys/Lista_5_Denys/Zad2.py
+++ JSP2021_Denys/Lista_5_Denys/Zad2.py
@@ -2,7 +2,7 @@ tlumacz={-1:'', 0:'zero', 1:'jeden', 2:'dwa', 3:'trzy', 4:'cztery', 5:'pięć', 
         9:'dziewięć', 10:'dziesięć', 11:'jedenaście', 12:'dwanaście', 13:'trzynaście', 14:'czternaście', 15:'piętnaście',
         16:'szesnaście', 17:'siedemnaście', 18:'osiemnaście', 19:'dziewiętnaście',20:'dwadzieścia',
         30:'trzydzieści', 40:'czterdzieści', 50:'pięćdziesiąt', 60:'sześćdziesiąt', 70:'siedemdziesiąt', 80:'osiemdziesiąt', 90:'dziewięćdziesiąt',
-        100:'sto', 200:'dwieście', 300:'trzysta', 400:'czterysta', 500:'pięćset', }#Przypożudkuje słową ich odpowednich w liczbach. Przed dwukropkie
+        100:'sto', 200:'dwieście', 300:'trzysta', 400:'czterysta', 500:'pięćset', 600:'sześćset', 700:'siedemset', 800:'osiemset', 900:'dziewięćset', 1000:'tysiąc'}#Przypożudkuje słową ich odpowednich w liczbach. Przed dwukropkie
                                                                  # jesy keyname, a po wartość jej przypisana
 
 def zamiana(tys,set,dzi,jed):
@@ -29,17 +29,17 @@ def zamiana(tys,set,dzi,jed):
 
 
 x = int(input("Podaj słownie liczbę: "))
-if (x<10):
+if (x<20):
     d = -1 #ustawia wartość na pusta    
     c = -1 #ustawia wartość na pusta    
     a = -1 #ustawia wartość na pusta    
-    b = x%10# oblicza od 1-9
-if (x>=10 and x<100):
+    b = x # od 0 do 20
+if (x>=20 and x<100):
     d = -1 #ustawia wartość na pusta    
     c = -1 #ustawia wartość na pusta    
     a = int((x/10))*10# oblicza całkowite dziesiątki
     b = x%10# oblicza od 1-9
-if(x>=1000 and x<1000):
+if(x>=100 and x<1000):
      d = -1 #ustawia wartość na pusta    
      c = int((x/100))*100# oblicza całkowite setki
      a = int(((x%100)/10))*10# oblicza całkowite dziesiątki
@@ -50,5 +50,4 @@ if(x>=1000 and x<2000):
      a = int(((x%100)/10))*10# oblicza całkowite dziesiątki
      b = x%10 # oblicza od 1-9
      
-print(d,c,a,b)
 zamiana(d,c,a,b)
