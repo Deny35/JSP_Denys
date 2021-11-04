e248f0c0afe14b7f1f96730fa5d9112a5d0dfe2a
Deny35 <kdenys141@gmail.com>
2021-11-04 08:28:28 +0100

    04.11.2021

--- JSP2021_Denys/Lista_6_Denys/SzyfrCezara.py
+++ JSP2021_Denys/Lista_6_Denys/SzyfrCezara.py
@@ -2,13 +2,15 @@ def szyfrowanie(tekst, klucz):
     szyfr = ''
     for i in range (len(tekst)): 
         if (ord(tekst[i]) < 97 or ord(tekst[i]) >122) or (ord(tekst[i]) < 65 or ord(tekst[i]) >90):# sprawdza czy znak nie jest literą
+            print('tak')
             szyfr += chr(ord(tekst[i]))# zistawia bez zmian
         elif (ord(tekst[i]) >= 97 and ord(tekst[i]) <=122) or (ord(tekst[i]) >= 65 and ord(tekst[i]) <=90):# sprawdza czy znak jest literą
+            print('ta')
             if (ord(tekst[i]) > 97 and (ord(tekst[i]) + klucz > 122)) or (ord(tekst[i]) < 90 and (ord(tekst[i]) + klucz > 90)):# sprawdza czy po dodaniu klucza nie wychodzi za skale
-                print('tak')
+                print('t')
                 szyfr += chr((ord(tekst[i])) + klucz - 26)# zamienia litere i dodaje dodaje do ciągu
             else:
-                print('tak')
+                print('t')
                 szyfr += chr((ord(tekst[i])) + klucz)# zamienia litere i dodaje dodaje do ciągu
     return szyfr
                 
