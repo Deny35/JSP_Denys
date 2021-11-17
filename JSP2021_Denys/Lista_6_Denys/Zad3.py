542fa680c89d08d7483fb489357af5ec25502d3d
Deny35 <kdenys141@gmail.com>
2021-11-17 23:45:30 +0100

    17.11.2021

--- JSP2021_Denys/Lista_6_Denys/Zad3.py
+++ JSP2021_Denys/Lista_6_Denys/Zad3.py
@@ -1,18 +1,19 @@
-#def lookandsay(n):
-ciag = ""
-i = 1
-a = 0
-n = '11121'
-while i < len(n):
-    if n[i-1] != n[i]:
-        licz = n[a:i].count(n[a:i])
-        a = i- 1
-        licz = str(licz)
-        ciag  = ciag + licz + n[a]
-    i += i
-print(ciag)
+n=int(input("Podaj n wyrazow ciagu:"))
 
-#pierwszy  = "1"
-#for i in range(3):
-#    print (pierwszy)
-#    pierwszy = lookandsay(pierwszy)
+def lookAndSay(x):
+    result=[]
+    i=0
+    while i < len(x):
+        licznik=1
+        while i + 1 <len(x) and x[i] == x[i+1]:
+            i+=1
+            licznik+=1
+        result.append(str(licznik)+x[i])
+        i+=1
+    return ''.join(result)
+
+x="1"
+for i in range(n):
+    x=lookAndSay(x)
+    print(x)
+
