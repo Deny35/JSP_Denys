f14cfa93a14c1fe96f43404314621419da0ab200
Deny35 <kdenys141@gmail.com>
2021-11-25 08:23:55 +0100

    NEW

--- JSP2021_Denys/Lista_4_Denys/Zad5.py
+++ JSP2021_Denys/Lista_4_Denys/Zad5.py
@@ -1,9 +1,12 @@
 from itertools import permutations
 
-ilosc = int(input("ilosc elementow "))
+ilosc = int(input("ilosc elementow: "))
 lista = []
 for i in range(0,ilosc):
-    x = int(input())
+    if i == 0:
+        x = int(input('Podaj pierwszy:'))
+    else:
+        x = int(input('Podaj kolejny:'))
     lista.append(x)
 
 for x in permutations(lista):
