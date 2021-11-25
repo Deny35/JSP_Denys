ad42a70def715cc33bfed1852f07e0e33fe6a133
Deny35 <kdenys141@gmail.com>
2021-11-25 08:27:43 +0100

    NEW

--- JSP2021_Denys/Lista_4_Denys/Zad10.py
+++ JSP2021_Denys/Lista_4_Denys/Zad10.py
@@ -1,8 +1,11 @@
 x = int(input('Podaj pierwszą liczbe:'))
 y = int(input('Podaj drugą liczbe: '))
 
-while x%y != 0:
-   r = x%y
-   y = r
+if(x%y == 0):
+   r = y
+else:
+   while x%y != 0:
+      r = x%y
+      y = r
 
 print('NWD=',r)
