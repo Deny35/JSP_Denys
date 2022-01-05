e8a66009107a72650c879ea2d6198733684ff44d
Deny35 <kdenys141@gmail.com>
2022-01-05 07:30:48 +0100

    05.01.2022

--- JSP2021_Denys/Lista_8_Denys/Zad_2.py
+++ JSP2021_Denys/Lista_8_Denys/Zad_2.py
@@ -10,14 +10,14 @@ i = 0
 
 for file in os.listdir(path_encrypted):
     if fnmatch.fnmatch(file, 'plik_zaszyfrowany*.txt'):
-       print (file)
+       #print (file)
        key = file[17]
        if file[18] == 0:
            key = 10
-       print(key)
+       #print(key)
        file_text =  path_encrypted +'\\' + file
        encrypted_text = Open.open_file(file_text)
        unencrypted_text = SzyfrCezara.deszyfrowanie(encrypted_text, int(key))
-       print(unencrypted_text)
+       #print(unencrypted_text)
        Save.save_file_z2(unencrypted_text, key, path_encrypted)
     
