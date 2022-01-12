b06c72dbf4758389595c3e653c397b75b06cc4ac
Deny35 <kdenys141@gmail.com>
2022-01-12 10:21:46 +0100

    12.01.2022

--- JSP2021_Denys/Lista_10_Denys/Zad_2.py
+++ JSP2021_Denys/Lista_10_Denys/Zad_2.py
@@ -1,23 +1,11 @@
 def get_power_set(s):
   power_set=[[]]
   for elem in s:
-
     for sub_set in power_set:
-
       power_set=power_set+[list(sub_set)+[elem]]
   return power_set
 
-belo = [[],[3],[1],[2,3],[3]]
-print(belo.sort(key=len))
 
-ini_list = [[1, 2, 3], [1, 2], [1, 2, 3, 4],
-                [1, 2, 3, 4, 5], [2, 4, 6]]
-  
-
-print ("initial list", str(ini_list))
-  
-
-ini_list.sort(key = len)
-  
-
-print("final list", str(ini_list))
+x = get_power_set([1,2,3])
+x.sort(key=len)
+print(x)
