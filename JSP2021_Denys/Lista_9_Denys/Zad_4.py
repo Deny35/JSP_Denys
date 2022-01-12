22066bbeeaae6b0b786906ea3bc8a4cd135a3c11
Deny35 <kdenys141@gmail.com>
2022-01-12 18:03:57 +0100

    12.01.2022

--- JSP2021_Denys/Lista_9_Denys/Zad_4.py
+++ JSP2021_Denys/Lista_9_Denys/Zad_4.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 
+def addlabels(x,y):
+    for i in range(len(x)):
+        plt.text(i,y[i],y[i])
+
 language = ['Python',  'C', 'Java', 'C++', 'C#', 'Visual Basic', 'JavaScript', 'Assembly language', 'SQL', 'Swift' ]
 ratings = [12.9, 11.8, 10.2, 7.7, 6.4, 5.4, 2.3, 2.3, 1.8, 1.8]
 
 plt.figure(figsize=(18, 5))
 plt.bar(language, ratings)
+addlabels(language, ratings)
 plt.xlabel("Języki programowania")
-plt.ylabel("Popularność w procętach")
+plt.ylabel("Popularność w procętach w %")
 plt.show()
