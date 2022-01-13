22fb6ea1232d259a48fa935eb42dea7866f5b0a4
Deny35 <kdenys141@gmail.com>
2022-01-13 09:29:24 +0100

    12.01.2022

--- JSP2021_Denys/Lista_9_Denys/statystyka.py
+++ JSP2021_Denys/Lista_9_Denys/statystyka.py
@@ -13,14 +13,10 @@ import os
 def open_file(path):
    
    if not os.path.exists(path):
-        print("Nie istnieje")    
+        pass    
    else:
-        print(path)
-        print("Plik istnieje")
-        file_open = open(path, 'r', encoding='utf-8', errors='ignore') 
-        file_text = file_open.read()
-        file_open.close()
-        return file_text
+        while open(path):
+            return [int(x) for x in path]
   
 
 if len(sys.argv) == 2:
--- JSP2021_Denys/Wejsciowki/p.py
+++ JSP2021_Denys/Wejsciowki/p.py
@@ -1,13 +1,5 @@
-def get_power_set(s):
-  power_set=[[]]
-  for elem in s:
-    # iterate over the sub sets so far
-    for sub_set in power_set:
-      # add a new subset consisting of the subset at hand added elem
-      power_set=power_set+[list(sub_set)+[elem]]
-  return power_set
+import numpy
 
+dane = [1,2,8,34]
 
-x = get_power_set([1,2,3])
-x.sort(key=len)
-print(x)
+print(numpy.median(dane))
